c9d79b73d9faca376a7e2dceef2a98fb27886a5d
Deniz Ata ES <denizataes@hotmail.com>
2022-11-17 22:29:48 +0300

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Deniz Ata Eş on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
